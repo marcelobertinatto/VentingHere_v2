dc683e914414021c74fc141a1bfa880bf36e4a35
Marcelo <marcelobertinatto@hotmail.com>
2020-04-12 17:03:32 +0100

    adding config needs

---
+++ VentingHere/AutoMapper/UserLoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace VentingHere.AutoMapper
+{
+    public class UserLoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
